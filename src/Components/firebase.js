0fb2fd2658a9bfa96241365397f8986bba233e24
parkjs101 <trckstr4422@gmail.com>
2023-08-07 10:45:47 +0900

    update

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -8,14 +8,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAieSwLh53z2u_ZU11BDf3IM_wwFhZ4QEA",
-  authDomain: "prada-ea8a3.firebaseapp.com",
-  databaseURL: "https://prada-ea8a3-default-rtdb.firebaseio.com",
-  projectId: "prada-ea8a3",
-  storageBucket: "prada-ea8a3.appspot.com",
-  messagingSenderId: "919680343184",
-  appId: "1:919680343184:web:56802ee911365d70b70c15",
-  measurementId: "G-CHV6XMV0HL"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  databaseUrl:  process.env.REACT_APP_DATABASE_URL
 };
 
 // Initialize Firebase
